ba496934f970ab2bed88f797666e809c9b2fbc8e
Lisa Walley <lwalley@eol.org>
2012-09-14 13:20:26 -0400

    CAT-62 Temporarily hardcoding change to environment name in redis factory lib to work in production.

--- lib/redis_factory.rb
+++ lib/redis_factory.rb
@@ -27,7 +27,7 @@ class RedisFactory
     
     def current_config(prefix)
       prefix = prefix.to_s
-      rails_env = Rails.env rescue 'development'
+      rails_env = Rails.env rescue 'production'
       
       config_key = if prefix.empty?
                     rails_env
